1b16d0c1e643967da38ecc21f666c50c36c50ee4
xpl000d <alexandru.marian@hotmail.com>
2022-09-21 12:08:32 +0300

    Add files via upload

---
+++ DeployMstunnel.ps1
@@ -0,0 +1,26 @@
+﻿# Sign in to your Azure subscription
+$sub = Get-AzSubscription -ErrorAction SilentlyContinue
+if(-not($sub))
+{
+    Connect-AzAccount
+}
+
+# If you have multiple subscriptions, set the one to use
+#Select-AzSubscription -SubscriptionId "SUBSCRIPTIONID"
+
+#Create Azure Resource Group for MSTunnel 
+New-AzResourceGroup -Name FTCmstunnel -Location "North Europe"
+
+
+New-AzResourceGroup -Name $resourceGroupName -Location "$location"
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri "https://raw.githubusercontent.com/xpl000d/ftc/main/Ubuntu_dock.json"
+
+#Create NSG rules to allow TCP/UDP Inbound/Outbound traffic to the VM 
+Get-AzNetworkSecurityGroup -Name "FTC-NSG" -ResourceGroupName "FTCmstunnel" | Add-AzNetworkSecurityRuleConfig -Name "Allow-TCP-443" -Description "Allow-TCP-443" -Access "Allow" -Protocol "Tcp" -Direction "Inbound" -Priority 100 -SourceAddressPrefix "*" -SourcePortRange "*" -DestinationAddressPrefix "*" -DestinationPortRange "443" | Set-AzNetworkSecurityGroup
+Get-AzNetworkSecurityGroup -Name "FTC-NSG" -ResourceGroupName "FTCmstunnel" | Add-AzNetworkSecurityRuleConfig -Name "Allow-UDP-443" -Description "Allow-UDP-443" -Access "Allow" -Protocol "UDP" -Direction "Inbound" -Priority 101 -SourceAddressPrefix "*" -SourcePortRange "*" -DestinationAddressPrefix "*" -DestinationPortRange "443" | Set-AzNetworkSecurityGroup
+Get-AzNetworkSecurityGroup -Name "FTC-NSG" -ResourceGroupName "FTCmstunnel" | Add-AzNetworkSecurityRuleConfig -Name "Allow-TCP-443" -Description "Allow-TCP-443" -Access "Allow" -Protocol "Tcp" -Direction "Outbound" -Priority 102 -SourceAddressPrefix "*" -SourcePortRange "*" -DestinationAddressPrefix "*" -DestinationPortRange "443" | Set-AzNetworkSecurityGroup
+
+
+
+
+
