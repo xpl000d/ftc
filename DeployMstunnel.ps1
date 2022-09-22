f5d357f45ac446cfebf0ef0d2da5fbae45d497ac
xpl000d <alexandru.marian@hotmail.com>
2022-09-22 15:51:01 +0300

    Update DeployMstunnel.ps1

--- DeployMstunnel.ps1
+++ DeployMstunnel.ps1
@@ -1,7 +1,3 @@
-##Install Azure module 
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -Force
-
 
 #Deploy Linux VM - Ubuntu 18.08 with Docker CE for Mstunnel deployment
 # Sign in to your Azure subscription
